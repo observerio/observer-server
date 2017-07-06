f564e88cbfedf8d0349b0ca2a59e0493f3f715c1
Alexandr Korsak <alex.korsak@gmail.com>
2017-07-06 17:09:35 +0300

    Use docker-ce

--- deployment/k8s-provision/service/kubernetes/scripts/install.sh
+++ deployment/k8s-provision/service/kubernetes/scripts/install.sh
@@ -5,6 +5,14 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial-unstable main
 EOF
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
 apt-get update
-apt-get install -y docker.io
+apt-get install -y docker-ce
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
