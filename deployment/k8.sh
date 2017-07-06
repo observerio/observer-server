fbcf1dcf89a863a52ed52f53d54e5e6813678005
Alexandr Korsak <alex.korsak@gmail.com>
2017-07-06 15:12:23 +0300

    Add docker registry secret yaml

--- deployment/k8.sh
+++
@@ -1,2 +0,0 @@
-# required to have auth details to have access to docker private registry
-kubectl create secret docker-registry regsecret --docker-server=<your-registry-server> --docker-username=<your-name> --docker-password=<your-pword> --docker-email=<your-email>
