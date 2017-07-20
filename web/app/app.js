98e8cefa5e56ac0283228f790666709baeef3f3b
Kirill Shaplyko <k.shaplyko@gmail.com>
2017-07-20 12:19:33 +0300

    Ember 2.14.0

--- web/app/app.js
+++ web/app/app.js
@@ -1,9 +1,11 @@
-import Ember from "ember";
-import Resolver from "./resolver";
-import loadInitializers from "ember-load-initializers";
-import config from "./config/environment";
+import Ember from 'ember';
+import Resolver from './resolver';
+import loadInitializers from 'ember-load-initializers';
+import config from './config/environment';
 
 let App;
+
+Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
