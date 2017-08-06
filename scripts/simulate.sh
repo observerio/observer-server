1608fdd9616435dc1d5848211469e0b98a27fc7e
Alexandr Korsak <alex.korsak@gmail.com>
2017-08-06 17:57:44 +0300

    Use 4001 for ws

--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-TCP_HOST=${TCP_HOST:-localhost}
-TCP_PORT=6667
-WEB_PORT=8080
+HTTP_HOST=${HTTP_HOST:-https://observer.rubyforce.co/api}
+TCP_HOST=observer.rubyforce.co
+TCP_PORT=6666
 
 SLEEP_TIME=20
 COMMAND_SLEEP_TIME=5
 
-until $(curl --output /dev/null --silent --fail -X GET http://$TCP_HOST:$WEB_PORT/alive); do
+until $(curl --output /dev/null --silent --fail -X GET $HTTP_HOST/alive); do
     printf '.'
     sleep 5
 done
 
-# KEY=`curl $TCP_HOST:$WEB_PORT/users/tokens | jq .token | sed 's/"\([^"]*\)"/\1/'`
-KEY='testtesttest'
-`curl -H "Content-Type: application/json" -d "'{"token":"'$KEY'"}'" -X POST "http://$TCP_HOST:$WEB_PORT/users/tokens"`
+KEY=${KEY:-testtesttest}
+KEY=`curl $HTTP_HOST/users/tokens | jq .token | sed 's/"\([^"]*\)"/\1/'`
+`curl -H "Content-Type: application/json" -d "'{"token":"'$KEY'"}'" -X POST "http://$HTTP_HOST/api/users/tokens"`
 
 while true
 do
