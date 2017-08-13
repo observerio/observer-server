2d5dd146515e76f848dee670817b0d98952fa177
Alexandr Korsak <alex.korsak@gmail.com>
2017-08-13 23:35:14 +0300

    stored token -> dashboard navigation

--- web/app/routes/index.js
+++ web/app/routes/index.js
@@ -1,7 +1,24 @@
 import Ember from "ember";
+import { storageFor } from 'ember-local-storage';
+
+const { get } = Ember;
 
 export default Ember.Route.extend({
+  session: storageFor('session'),
+
   beforeModel() {
-    this.transitionTo("login");
+    let session = get(this, 'session');
+
+    if (this.isAuthenticated()) {
+      let token = get(session, 'token');
+      this.transitionTo("dashboard", { token: token }) ;
+    } else {
+      this.transitionTo("login");
+    }
+  },
+
+  isAuthenticated: function() {
+    let session = get(this, 'session');
+    return get(session, 'token') != null;
   }
 });
